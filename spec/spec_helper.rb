9c5dfc4abcf0762c965569b670c449a22fe98038
Zach Schneider <schneidmaster@gmail.com>
2014-12-21 20:26:29 -0600

    Force RAILS_ENV to 'test' when running rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'simplecov'
 require 'coveralls'
@@ -29,14 +29,6 @@ ActiveRecord::Migration.maintain_test_schema!
 SimpleCov.start
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
   # Include FactoryGirl helper methods
   config.include FactoryGirl::Syntax::Methods
 
