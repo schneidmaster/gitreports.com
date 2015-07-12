15e8d63ebf9a432db48e809ceb5570fc7cb077e4
Zach Schneider <schneidmaster@gmail.com>
2015-07-12 15:43:32 -0500

    Move coverage earlier in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] = 'test'
-require File.expand_path('../../config/environment', __FILE__)
-
 if ENV['CIRCLE_ARTIFACTS']
   require 'codeclimate-test-reporter'
   CodeClimate::TestReporter.start
@@ -11,6 +7,9 @@ else
     add_filter '/workers/'
   end
 end
+
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../../config/environment', __FILE__)
 
 require 'rspec/rails'
 require 'capybara/rspec'
