8d294f5c4253374254bd05535832085bf4e9c128
Scott Ouellette <scottx611x@gmail.com>
2017-07-31 22:21:11 -0400

    This looks broken? Removing duplicate `ActiveRecord`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730214412) do
 ActiveRecord::Schema.define(version: 20170731231431) do
 
   create_table "organizations", force: :cascade do |t|
