08f098c4b5bdbedb9b77de552ef3805402306efe
Scott Ouellette <scottx611x@gmail.com>
2017-07-31 22:22:01 -0400

    Fix what Codeclimate yelled at me for

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,21 +1,21 @@
 class NotificationMailer < ActionMailer::Base
   default from: 'no-reply@gitreports.com'
 
-  def issue_submitted_email(repo_id, issue_id, submitter_info={})
+  def issue_submitted_email(repo_id, issue_id, submitter_info = {})
     defaults = {
-        @submitter_name => nil,
-        @submitter_email => nil
+      @submitter_name => nil,
+      @submitter_email => nil
     }
 
-    @submitter_info = defaults.merge(options)
+    @submitter_info = defaults.merge(submitter_info)
 
     @repository = Repository.find(repo_id)
     @issue_id = issue_id
 
     @subject = "New Issue Submitted to #{@repository.name}"
 
-    if @submitter_info[:submitter_name].present? and @submitter_info[:submitter_email].present?
-        @subject = "#{@subject} by #{@submitter_info[:submitter_name]}: #{@submitter_info[:submitter_email]}"
+    if @submitter_info[:submitter_name].present? && @submitter_info[:submitter_email].present?
+      @subject = "#{@subject} by #{@submitter_info[:submitter_name]}: #{@submitter_info[:submitter_email]}"
     end
 
     mail to: @repository.notification_emails, subject: @subject
