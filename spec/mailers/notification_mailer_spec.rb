ffd3043337cfc6df89f57aedb3c6a2a2fec3d962
Scott Ouellette <scottx611x@gmail.com>
2017-07-31 22:56:16 -0400

    Actually fix tests

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -8,12 +8,12 @@ describe NotificationMailer do
       expect(subject.body).to have_content('New issue submitted to repository CoolCode!')
     end
 
-    let!(:repository) { create :repository, name: 'CoolCode', owner: 'CoolOrg', notification_emails: 'joe@email.com', include_submitter_email: true}
+    let!(:repository) { create :repository, name: 'CoolCode', owner: 'CoolOrg', notification_emails: 'joe@email.com', include_submitter_email: true }
 
     subject { NotificationMailer.issue_submitted_email(repository.id, 1, submitter_name: 'Scott', submitter_email: 'Scott@Scott.com') }
 
     it 'sends notification mail with submitter information in subject' do
-      expect(subject).to have_content("Subject: New Issue Submitted to CoolCode by Scott: Scott@Scott.com")
+      expect(subject).to have_content('Subject: New Issue Submitted to CoolCode by Scott: Scott@Scott.com')
     end
   end
 end
