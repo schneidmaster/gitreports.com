6360551ca2955da2204d45d7a0d807b0f4286b4d
Scott Ouellette <scottx611x@gmail.com>
2017-07-31 21:37:23 -0400

    Provide optional `submitter_info` and add logic to adjust subject when necessary

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,23 @@
 class NotificationMailer < ActionMailer::Base
   default from: 'no-reply@gitreports.com'
 
-  def issue_submitted_email(repo_id, issue_id)
+  def issue_submitted_email(repo_id, issue_id, submitter_info={})
+    defaults = {
+        @submitter_name => nil,
+        @submitter_email => nil
+    }
+
+    @submitter_info = defaults.merge(options)
+
     @repository = Repository.find(repo_id)
     @issue_id = issue_id
 
-    mail to: @repository.notification_emails, subject: "New Issue Submitted to #{@repository.name}"
+    @subject = "New Issue Submitted to #{@repository.name}"
+
+    if @submitter_info[:submitter_name].present? and @submitter_info[:submitter_email].present?
+        @subject = "#{@subject} by #{@submitter_info[:submitter_name]}: #{@submitter_info[:submitter_email]}"
+    end
+
+    mail to: @repository.notification_emails, subject: @subject
   end
 end
