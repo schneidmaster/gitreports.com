62c33d1fc8139cd5c905ea4b813d4b5bbba871d6
Zach Schneider <zach@schneider.dev>
2020-06-08 09:48:00 -0500

    Ignore some useless exceptions from Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,15 @@ if Rails.env.production?
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_DSN']
 
-    config.excluded_exceptions = ['GithubRateLimitError']
+    config.excluded_exceptions = [
+      'GithubRateLimitError',
+
+      # Mostly bots trying WordPress URLs.
+      'ActionController::RoutingError',
+
+      # Revoked/expired credentials.
+      'Octokit::Forbidden',
+      'Octokit::Unauthorized'
+    ]
   end
 end
